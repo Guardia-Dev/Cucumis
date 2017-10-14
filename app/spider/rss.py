ab1b05065a0f084f33a5392ccee01599522c84e3
Desgard_Duan <gua@desgard.com>
2017-10-15 00:42:30 +0800

    Complete rss tools for scraw spider
    Signed-off-by: Desgard_Duan <gua@desgard.com>

--- app/spider/rss.py
+++ app/spider/rss.py
@@ -1,10 +1,15 @@
 import feedparser
 import re
 
+
 feed_url_list = {
     'Guardia': 'http://www.desgard.com/feed.xml',
     'Draveness': 'https://draveness.me/feed.xml',
-    'test': 'hehheheh',
+    'Bestswifter': 'https://bestswifter.com/rss/',
+    '唐巧': 'http://blog.devtang.com/atom.xml',
+    'mikeash': 'https://www.mikeash.com/pyblog/rss.py?mode=fulltext',
+    '腾讯科恩实验室官方博客': 'http://keenlab.tencent.com/zh/atom.xml',
+    '阿里中间件团队博客': 'http://jm.taobao.org/atom.xml',
 }
 
 def get_all_urls():
@@ -14,7 +19,7 @@ def get_all_urls():
     return res
 
 def parser_2_post(list):
-    regex = r"https*://.+feed.xml"
+    regex = r"https*://.+"
     feed_pattern = re.compile(regex)
     for url in list:
         # 正则检查一下 feed 地址的合法性
@@ -23,8 +28,35 @@ def parser_2_post(list):
             continue
 
         # 使用 feedparser 工具来爬取文章信息
+        list_xml = feedparser.parse(url)
+        re_serialize(url, list_xml)
 
 
+def re_serialize(url, rss_json):
+
+    # 博客信息
+    # Title 获取自己拟定的字典中取值
+    re_feed_url_list = {v:k for k,v in feed_url_list.items()}
+    blog_name = re_feed_url_list[url]
+    print(blog_name)
+
+    post_list = rss_json['entries']
+
+    # 文章列表
+    # 只获取最新的两篇
+
+    cnt = 0
+    for post in post_list:
+        if cnt is 2:
+            break
+        cnt += 1
+
+        title = post['title']
+        link = post['link']
+        published = post['published']
+        print(title, link, published)
+
+    print('\n')
 
 if __name__ == '__main__':
     parser_2_post(get_all_urls())
