6125767dbd115d41bf2c87071f640a0a443381d7
Gua <gua@desgard.com>
2018-07-24 14:56:42 +0800

    Update const.py

--- app/spider/const.py
+++ app/spider/const.py
@@ -1,18 +1,21 @@
 
 
 feed_url_list = {
-    'Guardia': 'http://www.desgard.com/feed.xml',
-    'Draveness': 'https://draveness.me/feed.xml',
-    'satanwoo':'http://satanwoo.github.io/atom.xml',
-    '玉令天下':'http://yulingtianxia.com/atom.xml',
-    'Bestswifter': 'https://bestswifter.com/rss/',
-    'Halfrost\'s Field': 'https://halfrost.com/rss/',
+    # 'Guardia': 'http://www.desgard.com/feed.xml',
+    # 'Draveness': 'https://draveness.me/feed.xml',
+    # 'satanwoo':'http://satanwoo.github.io/atom.xml',
+    # '玉令天下':'http://yulingtianxia.com/atom.xml',
+    # 'Bestswifter': 'https://bestswifter.com/rss/',
+    # 'Halfrost\'s Field': 'https://halfrost.com/rss/',
+    # '阿里中间件团队博客': 'http://jm.taobao.org/atom.xml',
+    # '陈鹏个人博客':'http://chenpeng.info/feed',
+    # 'Amywushu': 'https://amywushu.github.io/atom.xml',
+    # 'AloneMonkey 庆哥': 'http://www.alonemonkey.com/atom.xml',
     'mikeash': 'https://www.mikeash.com/pyblog/rss.py?mode=fulltext',
-    '阿里中间件团队博客': 'http://jm.taobao.org/atom.xml',
-    '陈鹏个人博客':'http://chenpeng.info/feed',
-    'Amywushu': 'https://amywushu.github.io/atom.xml',
-    'AloneMonkey 庆哥': 'http://www.alonemonkey.com/atom.xml',
-
+    'khanlou': 'http://khanlou.com/feed/',
+    'machinethink': 'http://machinethink.net/blog/index.xml',
+    'ericasadun': 'https://ericasadun.com/feed/',
+    'machinethink':'http://machinethink.net/blog/index.xml',
 }
 
 rss_datetime_format_str = "%a, %d %b %Y %H:%M:%S %z"
